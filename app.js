844f3834d0aebf01142bb2ab92e2c349ed2757c1
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-06-15 14:13:52 +0300

    I converted two pages to ejs files. I wrote it to make it clear which page is open in the header section, at the bottom of that page.

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set("view engine","ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.status(200).render('index',{
+    page_name:"index"
+  });
 });
+//page_name yazmamızın nedeni header kısmında hangi sayfa açıksa o sayfanın altında açık olduğu belli olsun diye yazdım
+//Sonradan navigation'da page_name == about ise active ediyoruz (About örnek hangi sayfadaysak öyle yapacağız)
+app.get('/about', function (req, res) {
+  res.status(200).render('about',{
+    page_name:"about" 
+  });
+});
+
 
 const port = 3000;
 
