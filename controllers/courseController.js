e90ea1a7d6d0babff545f5a77c8d45beb8238c33
nurettinyavuz <Nurettinyavuz.js@gmail.com>
2023-06-17 22:06:56 +0300

    I activated the inside of the courses, that is, I activated the description, date and course name entered by the user and made cosmetic adjustments.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,15 +1,15 @@
 const Course = require('../models/Course');
 
 exports.createCourse = async (req, res) => {
-try {  
-  const course = await Course.create(req.body); //Kurs açmak için form'dan bilgileri alacağız
-  //try-catch yapmamızın nedeni hatayı yakalamak için
+  try {
+    const course = await Course.create(req.body); //Kurs açmak için form'dan bilgileri alacağız
+    //try-catch yapmamızın nedeni hatayı yakalamak için
     res.status(201).json({
       //Oluşturulan yeni kursu template'e göndermiyoruz json dosyasında saklıyacaağız
       status: 'success',
       course,
     });
-  } catch(error){
+  } catch (error) {
     res.status(400).json({
       //Hatalı request gönderilirse dönecek
       //Oluşturulan yeni kursu template'e göndermiyoruz json dosyasında saklıyacaağız
@@ -19,25 +19,44 @@ try {
   }
 };
 
+//Listelemek için
 exports.getAllCourse = async (req, res) => {
-  try {//try-catch yapmamızın nedeni hatayı yakalamak için
+  try {
+    //try-catch yapmamızın nedeni hatayı yakalamak için
     const courses = await Course.find(); //Tüm kursları sıraladı
-  /*    res.status(200).json({
+    /*    res.status(200).json({
         //Oluşturulan yeni kursu template'e göndermiyoruz json dosyasında saklıyacaağız
         status: 'success',
         courses,
       });
      */
-      res.status(200).render('courses',{
-        courses,
-        page_name:'courses'
-      })
-    } catch(error){
-      res.status(400).json({
-        //Hatalı request gönderilirse dönecek
-        //Oluşturulan yeni kursu template'e göndermiyoruz json dosyasında saklıyacaağız
-        status: 'fail',
-        error,
-      });
-    }
-  };
+    res.status(200).render('courses', {
+      courses,
+      page_name: 'courses',
+    });
+  } catch (error) {
+    res.status(400).json({
+      //Hatalı request gönderilirse dönecek
+      //Oluşturulan yeni kursu template'e göndermiyoruz json dosyasında saklıyacaağız
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+//TEKİL KURS
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({ _id: req.params.id });
+
+    res.status(200).render('course', {//Burada yazan isim ejs sayfası
+      course,
+      page_name: 'courses',
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -6,6 +6,8 @@ const router = express.Router();
 //Form dolduracağız onu göndereceğimiz için post kullandık
 router.route('/').post(courseController.createCourse);//courseController'daki createCourse'a gidecek
 router.route('/').get(courseController.getAllCourse);
+router.route('/:id').get(courseController.getCourse);
+
 
 
 //http://localhost:3000/courses 
